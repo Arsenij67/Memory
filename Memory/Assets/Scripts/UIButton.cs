297191f6b3a10961e5c79ecfb10c6001ddc02366
Arsenij67 <96657896+Arsenij67@users.noreply.github.com>
2022-09-27 03:26:40 +0300

    Update

---
+++ Memory/Assets/Scripts/UIButton.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    public SceneController Controller;
+    [SerializeField] private string targetMessage;
+    public Color hightlightColor = Color.cyan;
+
+    public void OnMouseOver()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = hightlightColor;
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        if (sprite != null)
+        {
+            sprite.color = Color.white;
+        
+        }
+    
+
+
+    }
+    public void OnMouseDown()
+    {
+        transform.localScale = new Vector3(1.1f,1.1f,1.1f);
+    }
+    public void OnMouseUp()
+    {
+        transform.localScale = Vector3.one;
+       
+
+        {
+
+            Controller.SendMessage(targetMessage);
+        
+        }
+    }
+}   
